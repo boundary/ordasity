5a8755172821210a555fa47db1b216da2c9cda13
C. Scott Andreas <s@boundary.com>
2012-02-19 11:41:20 -0800

    Flip ClusterListener from a trait to an abstract class, as 'mixing it in' is not meaningful. Update metrics import path.

--- src/main/scala/com/boundary/ordasity/ClusterListener.scala
+++ src/main/scala/com/boundary/ordasity/ClusterListener.scala
@@ -1,19 +1,19 @@
 package com.boundary.ordasity
 
-import com.yammer.metrics.Meter
+import com.yammer.metrics.scala.Meter
 import com.twitter.common.zookeeper.ZooKeeperClient
 
-trait Listener {
+abstract class Listener {
   def onJoin(client: ZooKeeperClient)
   def onLeave()
   def shutdownWork(workUnit: String)
 }
 
-trait SmartListener extends Listener {
+abstract class SmartListener extends Listener {
   def startWork(workUnit: String, meter: Meter)
 }
 
-trait ClusterListener extends Listener {
+abstract class ClusterListener extends Listener {
   def startWork(workUnit: String)
 }
 
